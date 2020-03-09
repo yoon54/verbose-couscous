76cf2d1cc9f5d5029af18ee2ba9dad14357506a7
Henry Yoon <henry.yoon@hotmail.com>
2020-03-09 12:27:15 -0400

    initial commit

--- src/components/hookForm.js
+++ src/components/hookForm.js
@@ -5,7 +5,8 @@ const HookForm = props => {
         lastName: "",
         email: "",
         password: "",
-        confirmPassword: ""
+        confirmPassword: "",
+        submitted: false
     })
     const onChangeHandler = event => {
         setFormState({
@@ -15,11 +16,26 @@ const HookForm = props => {
     }
     const onSubmitHandler = event => {
         event.preventDefault();
-        console.log(formState);
+        setFormState({
+             ...formState,
+            submitted: true
+})
     }
+    let message;
+    if(formState.submitted){
+        message=<h1>You have submitted the form</h1>;
+    }else{
+        message=<h1>You have not yet submitted the form</h1>;
+    }
+    // if(formState.firstName.length > 2){
+    //     message=<h1>first name is submited!</h1>;
+    // }else{
+    //     message=<h1>first name needs to be longer than 2</h1>;
+    // }
     return(
         <>
             <div>
+                <p>{message}</p>
                 <form onSubmit={onSubmitHandler}>
                     <label>First Name</label>
                     <input type="text" name="firstName" onChange={onChangeHandler}/>
@@ -39,10 +55,15 @@ const HookForm = props => {
                     <input type="submit" />     
                 </form>
                 <p>First Name: {formState.firstName}</p>
+                {formState.firstName.length < 2 && <h4>first name should be 2 characters</h4> && formState.firstName.length > 0 ? <h4>first name should be 2 characters</h4> :true}
                 <p>Last Name: {formState.lastName}</p>
+                {formState.lastName.length < 2 && <h4>last name should be 2 characters</h4> && formState.lastName.length > 0? <h4>last name should be 2 characters</h4> :true}
                 <p>Email: {formState.email}</p>
+                {formState.email.length < 2 && <h4>email should be 2 characters</h4> && formState.email.length > 0? <h4>email should be 2 characters</h4> :true}
                 <p>Password: {formState.password}</p>
+                {formState.password.length < 8 && <h4>password should be 8 characters</h4> && formState.password.length > 0 ? <h4>password should be 8 characters</h4> :true}
                 <p>Confirm Password: {formState.confirmPassword}</p>
+                {formState.confirmPassword != formState.password && <h4>password should match</h4> ? <h4>password should match</h4> :true}
             </div>
         </>
     );
